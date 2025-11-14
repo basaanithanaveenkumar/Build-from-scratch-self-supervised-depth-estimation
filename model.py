02a278282a09ffec14dc03cabdc1a30cd3daa421
Naveenkumar <baseanithanaveenkumar1998@gmail.com>
2025-11-14 20:59:19 +0530

    added the tim based dino v3

---
+++ model.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+from PIL import Image
+import timm
+
+img = Image.open(urlopen(
+    'https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png'
+))
+
+model = timm.create_model(
+    'vit_huge_plus_patch16_dinov3_qkvb.lvd1689m',
+    pretrained=True,
+    features_only=True,
+)
+model = model.eval()
+
+# get model specific transforms (normalization, resize)
+data_config = timm.data.resolve_model_data_config(model)
+transforms = timm.data.create_transform(**data_config, is_training=False)
+
+output = model(transforms(img).unsqueeze(0))  # unsqueeze single image into batch of 1
+
+for o in output:
+    # print shape of each feature map in output
+    # e.g.:
+    #  torch.Size([1, 1280, 16, 16])
+    #  torch.Size([1, 1280, 16, 16])
+    #  torch.Size([1, 1280, 16, 16])
+
+    print(o.shape)
